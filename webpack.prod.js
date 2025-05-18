52f6ade6a361fd4996bc2434591dae3e92309745
Anshika Jaiswal <anshikajaiswal123987@gmail.com>
2025-05-19 02:59:09 +0530

    Fix: corrected webpack.prod.js for Netlify deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,22 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 process.env["NODE_ENV"] = "production";
 
-module.exports = merge([
-  common,
-  {
-    mode: "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
-        new CssMinimizerPlugin(),
-      ],
-    },
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
   },
-]);
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`, // extend default minimizers like Terser
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
+
